ae78ddc8ca55ff66d66919399b67e4549ad0faba
senan31122 <152394423+senan31122@users.noreply.github.com>
2023-11-29 14:29:48 +0300

    Add files via upload

---
+++ lab2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int plaka_kodu;
+    printf("Ilin plaka kodunu giriniz: ");
+    scanf("%d",&plaka_kodu);
+   
+    if  (plaka_kodu==6)
+         printf("ANKARA\n");
+    else if  (plaka_kodu==34)
+        printf("ISTANBUL\n");
+    else if  (plaka_kodu==35)
+        printf("IZMIR\n");
+    else if  (plaka_kodu==45)
+        printf("MANISA\n");
+    else
+        
+    return 0;
+        
+         
+}
